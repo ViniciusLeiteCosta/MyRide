1bb26554bb286f0fdd0c84f1f62b37d7e101109a
Leite | Vinicius Leite Costa <viniciusleitecosta@gmail.com>
2024-01-25 05:29:27 -0300

    Creating switch button in Form.tsx

--- MyRide-Website/src/components/form/Form.tsx
+++ MyRide-Website/src/components/form/Form.tsx
@@ -1,86 +1,86 @@
 import React, { useState, useEffect } from 'react';
-import { Container, TextField, Typography, Grid, Button, MenuItem, Select, InputLabel } from "@mui/material";
-import formImage from "../../assets/Popup image.png";
-import carSVG1 from "../../assets/Card Image1.svg";
-import carSVG2 from "../../assets/Card Image2.svg";
-import carSVG3 from "../../assets/Card Image3.svg";
-import carSVG4 from "../../assets/Card Image4.svg";
+import { Container, TextField, Typography, Grid, Button, MenuItem, Select, Switch} from '@mui/material';
+import formImage from '../../assets/Popup image.png';
+import carSVG1 from '../../assets/Card Image1.svg';
+import carSVG2 from '../../assets/Card Image2.svg';
+import carSVG3 from '../../assets/Card Image3.svg';
+import carSVG4 from '../../assets/Card Image4.svg';
 import countriesAndCities from '../../data/countries-and-cities.json';
 
 //Styles
 const form = {
-  maxWidth: "100%",
-  display: "flex",
-  paddingTop: "101px",
-  padding: "30px",
-  flexDirection: "column",
-  alignItems: "flex-start",
-  gap: "24px",
+  maxWidth: '100%',
+  display: 'flex',
+  paddingTop: '101px',
+  padding: '30px',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '24px',
 };
 
 const formHeader = {
-  display: "flex",
-  justifyContent: "flex-start",
-  marginTop: "30px",
+  display: 'flex',
+  justifyContent: 'flex-start',
+  marginTop: '30px',
   // border: 'solid red',
 };
 
 const defaultContainer = {
   // border: 'solid white',
-  background: "#282828",
-  borderRadius: "10px",
+  background: '#282828',
+  borderRadius: '10px',
 };
 
 const typographyStyle = {
-  display: "flex",
-  justifyContent: "flex-start",
+  display: 'flex',
+  justifyContent: 'flex-start',
 };
 
 const textfieldStyle = {
-  display: "flex",
-  justifyContent: "flex-start",
-  border: "solid purple",
-  margin: "24px 0 0 -30px",
+  display: 'flex',
+  justifyContent: 'flex-start',
+  // border: 'solid purple',
+  margin: '24px 0 0 -30px',
 
-  "& label.Mui-focused": {
-    color: "#FBA403",
+  '& label.Mui-focused': {
+    color: '#FBA403',
   },
-  "& .MuiInputLabel-root": {
-    color: "#FFFFFF",
+  '& .MuiInputLabel-root': {
+    color: '#FFFFFF',
   },
-  "&:hover .MuiInputLabel-root": {
-    color: "#FBA403",
+  '&:hover .MuiInputLabel-root': {
+    color: '#FBA403',
   },
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      transition: "0.5s",
-      borderColor: "#5d5d5d",
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      transition: '0.5s',
+      borderColor: '#5d5d5d',
     },
-    "&:hover fieldset": {
-      borderColor: "#FBA403",
+    '&:hover fieldset': {
+      borderColor: '#FBA403',
     },
-    "&.Mui-focused fieldset": {
-      borderColor: "#FBA403",
+    '&.Mui-focused fieldset': {
+      borderColor: '#FBA403',
     },
   },
 };
 
 const optionCard = {
-  display: "flex",
-  flexDirection: "column", 
-  margin: "5px",
-  width: "130px",
-  height: "80px",
-  border: "1px solid #FFF",
-  borderRadius: "4px",
-  cursor: "pointer",
+  display: 'flex',
+  flexDirection: 'column', 
+  margin: '5px',
+  width: '130px',
+  height: '80px',
+  border: '1px solid #FFF',
+  borderRadius: '4px',
+  cursor: 'pointer',
 };
 
 const carButtons = {
-  display: "flex",
-  flexDirection: "row",
-  marginLeft: "-60px",
-  b: "#FFF",
+  display: 'flex',
+  flexDirection: 'row',
+  marginLeft: '-60px',
+  b: '#FFF',
 };
 
 //If i need to use it
@@ -94,14 +94,18 @@ const carButtons = {
 
 const countrySelectStyle = {
 
-    display: "flex",
+    display: 'flex',
     textAlign: 'left', 
     flexDirection: 'left',
-    justifyContent: "flex-start",
-    // border: "solid red",
-    margin: "24px -25px 0 -54px",
+    justifyContent: 'flex-start',
+    // border: 'solid red',
+    margin: '24px -25px 0 -54px',
+    
+    '& fieldset': {
+        borderColor: '#5d5d5d', 
+      },
 
-    '&.MuiOutlinedInput-notchedOutline': {
+    '&.MuiOutlinedInput-notchedOutline': {  
         transition: '0.5s',
         borderColor: '#5d5d5d',
       },
@@ -127,29 +131,50 @@ const countrySelectStyle = {
 }
 
 const submitButtonStyle = {
-  display: "flex",
-  width: "200px",
-  height: "56px",
-  background: "#FBA403",
-  margin: "24px 0 30px -30px",
+  display: 'flex',
+  width: '200px',
+  height: '56px',
+  background: '#FBA403',
+  margin: '24px 0 30px -30px',
 };
 
+const switchStyle = {
+  '& .MuiSwitch-switchBase': {
+    color: '#FBA403', 
+  },
+  '& .MuiSwitch-switchBase.Mui-checked': {
+    color: '#FBA403', 
+  },
+  '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+    backgroundColor: '#FBA403', 
+  },
+  '& .MuiSwitch-track': {
+    backgroundColor: '#5d5d5d', 
+  },
+};
 
-const Form = () => {
+const Form: React.FC = () => {
+
+  const [ownsCar, setOwnsCar] = useState<boolean>(false);
+
+  const handleOwnsCarChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setOwnsCar(event.target.checked);
+  };
+
   return (
     <div>
       <Container style={form}>
         <Container style={defaultContainer}>
-          <Grid style={formHeader} container alignItems="center" spacing={2}>
+          <Grid style={formHeader} container alignItems='center' spacing={2}>
             <Grid item>
-              <img src={formImage} alt="Drive with MyRide" />
+              <img src={formImage} alt='Drive with MyRide' />
             </Grid>
 
             <Grid item>
               <Typography
                 style={typographyStyle}
-                variant="h5"
-                color={{ color: "#FBA403" }}
+                variant='h5'
+                color={{ color: '#FBA403' }}
               >
                 Drive with MyRide
               </Typography>
@@ -163,26 +188,27 @@ const Form = () => {
           <Container>
             <TextField
               sx={textfieldStyle}
-              id="outlined-basic"
-              label="Outlined"
-              variant="outlined"
-              label="Full Name"
-              alt="Full Name"
+              id='outlined-basic'
+              label='Outlined'
+              variant='outlined'
+              label='Full Name'
+              alt='Full Name'
             ></TextField>
 
             <TextField
               sx={textfieldStyle}
-              id="outlined-basic"
-              label="Outlined"
-              variant="outlined"
-              label="Email Address"
-              alt="Email Address"
+              id='outlined-basic'
+              label='Outlined'
+              variant='outlined'
+              label='Email Address'
+              alt='Email Address'
             ></TextField>
 
         <Container>
             <Select
-                labelId="country-label"
-                defaultValue=""
+            
+                labelId='country-label'
+                defaultValue=''
                 displayEmpty
                 sx={countrySelectStyle}
                 
@@ -190,7 +216,7 @@ const Form = () => {
                 'aria-label': 'Without country selected', 
                 }}
             >
-                <MenuItem value="" disabled>
+                <MenuItem value='' disabled>
                 Country
                 </MenuItem>
                 {Object.keys(countriesAndCities).map((country) => (
@@ -201,65 +227,72 @@ const Form = () => {
 
             <TextField
               sx={textfieldStyle}
-              id="outlined-basic"
-              label="Outlined"
-              variant="outlined"
-              label="City"
-              alt="City"
+              id='outlined-basic'
+              label='Outlined'
+              variant='outlined'
+              label='City'
+              alt='City'
             ></TextField>
 
             <TextField
               sx={textfieldStyle}
-              id="outlined-basic"
-              label="Outlined"
-              variant="outlined"
-              label="Referral Code"
-              alt="Referral Code"
+              id='outlined-basic'
+              label='Outlined'
+              variant='outlined'
+              label='Referral Code'
+              alt='Referral Code'
             ></TextField>
 
-            <Typography
-              style={{
-                display: "flex",
-                justifyContent: "flex-start",
-                margin: "24px 0 24px -30px",
-              }}
-              alt="I drive my own car"
-            >
-              I drive my own car
-            </Typography>
+        <Typography
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              margin: '24px 0 24px -30px',
+            }}
+            alt='I drive my own car'
+          >
+            I drive my own car
+            <Switch
+              sx={switchStyle}
+              checked={ownsCar}
+              onChange={handleOwnsCarChange}
+            />
+        </Typography>
+
 
             <Typography
               style={{
-                display: "flex",
-                justifyContent: "flex-start",
-                margin: "24px 0 16px -30px",
-                color: "#FBA403",
+                display: 'flex',
+                justifyContent: 'flex-start',
+                margin: '24px 0 16px -30px',
+                color: '#FBA403',
               }}
-              variant="h6"
-              alt="Select your car type"
+              variant='h6'
+              alt='Select your car type'
             >
               Select your car type
             </Typography>
 
             <Container style={carButtons}>
               <Button style={optionCard}>
-                <img alt="Sedan" src={carSVG1}></img>Sedan
+                <img alt='Sedan' src={carSVG1}></img>Sedan
               </Button>
               <Button style={optionCard}>
-                <img alt="SUV/Van" src={carSVG2}></img>SUV/Van
+                <img alt='SUV/Van' src={carSVG2}></img>SUV/Van
               </Button>
               <Button style={optionCard}>
-                <img alt="Semi Luxury" src={carSVG3}></img>Semi Luxury
+                <img alt='Semi Luxury' src={carSVG3}></img>Semi Luxury
               </Button>
               <Button style={optionCard}>
-                <img alt="Luxury Car" src={carSVG4}></img>Luxury Car
+                <img alt='Luxury Car' src={carSVG4}></img>Luxury Car
               </Button>
             </Container>
 
             <Button
-              variant="contained"
+              variant='contained'
               style={submitButtonStyle}
-              alt="Submit Button"
+              alt='Submit Button'
             >
               SUBMIT
             </Button>
